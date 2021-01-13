9aa8e83491882279ced1e05fb4e31706c25adab7
iamgrodrigues <iamgrodrigues@gmail.com>
2021-01-13 10:02:18 -0300

    build: Add jquery and bootstrap files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateBoardroomComponent } from './create-boardroom/create-boardroom.component';
+import { BoardroomDetailsComponent } from './boardroom-details/boardroom-details.component';
+import { BoardroomListComponent } from './boardroom-list/boardroom-list.component';
+import { UpdateBoardroomComponent } from './update-boardroom/update-boardroom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateBoardroomComponent,
+    BoardroomDetailsComponent,
+    BoardroomListComponent,
+    UpdateBoardroomComponent
   ],
   imports: [
     BrowserModule,
